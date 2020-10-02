4b70dae5a955444bec0de353801232c075a4cfdf
Vanessa McHale <vamchale@gmail.com>
2020-10-01 19:10:48 -0500

    comments?

--- src/Kempe/TypeSynthesis.hs
+++ src/Kempe/TypeSynthesis.hs
@@ -4,6 +4,12 @@ module Kempe.TypeSynthesis ( catTypes
 import           Kempe.AST
 
 -- should types of e.g. atoms be StackType a -> StackType a ?
+--
+-- alpha-equivalence (of 'StackType's?) (note it is quantified *only* on the "exterior" i.e.
+-- implicitly)
 
-catTypes :: StackType a -> StackType a -> StackType a
-catTypes _ _ = undefined
+-- | Given @x@ and @y@, return the 'StackType' of @xy@
+catTypes :: StackType a -- ^ @x@
+         -> StackType a -- ^ @y@
+         -> StackType a
+catTypes _ _ = undefined -- not actually easy because I need unification? :o
