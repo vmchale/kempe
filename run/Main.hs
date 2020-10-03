f9345670c93ac9130d12bb16c82bd89a6570ff36
Vanessa McHale <vamchale@gmail.com>
2020-10-03 16:16:18 -0500

    Compiler

--- run/Main.hs
+++ run/Main.hs
@@ -1,4 +1,46 @@
 module Main (main) where
 
+import qualified Data.Version        as V
+import           Options.Applicative
+import qualified Paths_kempe         as P
+
+data Command = TypeCheck !FilePath
+             | Compile !FilePath !FilePath
+
+run :: Command -> IO ()
+run (TypeCheck _) = pure ()
+run (Compile _ _) = pure ()
+
+kmpFile :: Parser FilePath
+kmpFile = argument str
+    (metavar "FILE"
+    <> help "Source file"
+    <> kmpCompletions)
+
+exeFile :: Parser FilePath
+exeFile = argument str
+    (metavar "OUTPUT"
+    <> help "File output")
+
+kmpCompletions :: HasCompleter f => Mod f a
+kmpCompletions = completer . bashCompleter $ "file -X '!*.kmp' -o plusdirs"
+
+commandP :: Parser Command
+commandP = hsubparser
+    (command "typecheck" (info tcP (progDesc "Type-check module contents")))
+    <|> compileP
+    where
+        tcP = TypeCheck <$> kmpFile
+        compileP = Compile <$> kmpFile <*> exeFile
+
+wrapper :: ParserInfo Command
+wrapper = info (helper <*> versionMod <*> commandP)
+    (fullDesc
+    <> progDesc "Kempe language compiler, x86_64 and Aarch64 backend"
+    <> header "Kempe - a stack-based language")
+
+versionMod :: Parser (a -> a)
+versionMod = infoOption (V.showVersion P.version) (short 'V' <> long "version" <> help "Show version")
+
 main :: IO ()
-main = putStrLn "incomplete."
+main = run =<< execParser wrapper
