de525561640d61dafa0d0f79351f7a15cebe19de
Vanessa McHale <vamchale@gmail.com>
2020-10-09 08:37:28 -0500

    bits

--- src/Kempe/Error.hs
+++ src/Kempe/Error.hs
@@ -7,7 +7,7 @@ import           Kempe.AST
 import           Prettyprinter (Pretty (pretty), squotes, (<+>))
 
 -- reject mutually recursive types? idk :p
-data Error a = MismatchedTypes a (KempeTy a) (KempeTy a)
+data Error a = MismatchedTypes a (KempeTy a) (KempeTy a) -- TODO: include atom "expression?"
 
 instance Pretty (Error a) where
     pretty (MismatchedTypes _ ty ty') = "could not match type" <+> squotes (pretty ty) <+> "with type" <+> squotes (pretty ty')
--- src/Kempe/TypeSynthesis.hs
+++ src/Kempe/TypeSynthesis.hs
@@ -15,9 +15,10 @@ import           Lens.Micro.Mtl      (modifying)
 
 type TyEnv a = IM.IntMap (KempeTy a)
 
-data TyState a = TyState { maxU    :: Int -- ^ For renamer
-                         , tyEnv   :: TyEnv a
-                         , renames :: IM.IntMap Int
+data TyState a = TyState { maxU             :: Int -- ^ For renamer
+                         , tyEnv            :: TyEnv a
+                         , renames          :: IM.IntMap Int
+                         , constructorTypes :: IM.IntMap Int
                          }
 
 maxULens :: Lens' (TyState a) Int
@@ -31,18 +32,18 @@ dummyName n = do
 
 type TypeM a = State (TyState a)
 
+-- TODO: take constructor types as an argument?..
 runTypeM :: TypeM a x -> x
-runTypeM = flip evalState (TyState 0 mempty mempty)
+runTypeM = flip evalState (TyState 0 mempty mempty mempty)
 
 -- alpha-equivalence (of 'StackType's?) (note it is quantified *only* on the "exterior" i.e.
 -- implicitly) -> except we have to then "back-instantiate"? hm
 
--- how does one scope unification "back"? (tardis monad but I don't want it to
--- hang indefinitely...)
---
 -- monomorphization
 
 -- dip-ify?
+
+-- renameStackType? or maybe j substitute?
 
 typeOfBuiltin :: BuiltinFn -> TypeM () (StackType ())
 typeOfBuiltin Drop = do
@@ -52,6 +53,11 @@ typeOfBuiltin Swap = do
     aN <- dummyName "a"
     bN <- dummyName "b"
     pure $ StackType (S.fromList [aN, bN]) [TyVar () aN, TyVar () bN] [TyVar () bN, TyVar () aN]
+
+-- maybe constraints? e.g. ("a" = "b") and (3 = "a")
+-- but maybe simpler since no function types? lol
+--
+-- so I can literally just check it's 3 and then pass that back lololol
 
 -- | Given @x@ and @y@, return the 'StackType' of @x y@
 catTypes :: StackType a -- ^ @x@
