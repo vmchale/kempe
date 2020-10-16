70a226674c7403ed2c46d205cff441f6ac3e6343
Vanessa McHale <vamchale@gmail.com>
2020-10-15 21:10:30 -0500

    prune

--- src/Kempe/Error.hs
+++ src/Kempe/Error.hs
@@ -15,10 +15,9 @@ import           Kempe.Name
 import           Prettyprinter     (Pretty (pretty), comma, squotes, (<+>))
 
 -- reject mutually recursive types? idk :p
-data Error a = MismatchedTypes a (KempeTy a) (KempeTy a) -- TODO: include atom "expression?"
-             | PoorScope a (Name a)
+data Error a = PoorScope a (Name a)
              | MismatchedLengths a (StackType a) (StackType a)
-             | UnificationFailed a (KempeTy a) (KempeTy a)
+             | UnificationFailed a (KempeTy a) (KempeTy a) -- TODO: include atom expression?
              | TyVarExt a
              deriving (Generic, NFData)
 
@@ -26,7 +25,6 @@ instance (Pretty a) => Show (Error a) where
     show = show . pretty
 
 instance Pretty (Error a) where
-    pretty (MismatchedTypes _ ty ty')    = "could not match type" <+> squotes (pretty ty) <+> "with type" <+> squotes (pretty ty')
     pretty (PoorScope _ n)               = "name" <+> squotes (pretty n) <+> "not in scope"
     pretty (MismatchedLengths _ st0 st1) = "mismatched type lengths" <+> pretty st0 <> comma <+> pretty st1
     pretty (UnificationFailed _ ty ty')  = "could not unify type" <+> squotes (pretty ty) <+> "with" <+> squotes (pretty ty')
