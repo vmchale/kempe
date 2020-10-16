5a3ac60d8ac551854a9a62a78f995e2e176c4bb3
Vanessa McHale <vamchale@gmail.com>
2020-10-16 15:38:03 -0500

    little shuffling around

--- src/Kempe/Monomorphize.hs
+++ src/Kempe/Monomorphize.hs
@@ -1,10 +1,21 @@
 module Kempe.Monomorphize ( exports
+                          , mkModuleMap
                           ) where
 
-import           Data.Maybe (mapMaybe)
-import qualified Data.Set   as S
+import qualified Data.IntMap  as IM
+import           Data.Maybe   (mapMaybe)
+import qualified Data.Set     as S
 import           Kempe.AST
 import           Kempe.Name
+import           Kempe.Unique
+
+type ModuleMap a b = IM.IntMap (KempeDecl a b)
+
+mkModuleMap :: Module a b -> ModuleMap a b
+mkModuleMap = IM.fromList . mapMaybe toInt
+    where toInt d@(FunDecl _ (Name _ (Unique i) _) _ _ _)   = Just (i, d)
+          toInt d@(ExtFnDecl _ (Name _ (Unique i) _) _ _ _) = Just (i, d)
+          toInt _                                           = Nothing
 
 exports :: Module a b -> S.Set (Name b)
 exports = S.fromList . mapMaybe exportsDecl
