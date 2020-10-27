40c4292cdfed77a960f5a96aa96038ebc62b85f1
Vanessa McHale <vanessa.mchale@bitnomial.com>
2020-10-27 12:46:51 -0500

    splitmix?

---
+++ benchmarks/splitmix64.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+uint64_t next(uint64_t x) {
+	uint64_t z = (x += 0x9e3779b97f4a7c15);
+	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+	return z ^ (z >> 31);
+}
