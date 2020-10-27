cc780aa9abf4d86a8a80c43940137ceb95e308ac
Vanessa McHale <vamchale@gmail.com>
2020-10-27 13:11:24 -0500

    I think correct it

--- benchmarks/splitmix64.c
+++ benchmarks/splitmix64.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
 
-uint64_t next(uint64_t x) {
+// modified to have ""multiple return"" since C doesn't really have that
+uint64_t next(uint64_t x, uint64_t* y) {
 	uint64_t z = (x += 0x9e3779b97f4a7c15);
+        *y = x;
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
 	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
 	return z ^ (z >> 31);
