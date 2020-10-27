1d9bfde532bdb76d572d3f05ad5e2844b426d8a4
Vanessa McHale <vamchale@gmail.com>
2020-10-27 11:05:20 -0500

    Add ty bench

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -17,16 +17,19 @@ main =
                       [ bench "lex"   $ nf lexKempe contents
                       , bench "parse" $ nf parse contents
                       ]
-                , env parsedM $ \p ->
+                , env forTyEnv $ \ ~(p, s) ->
                     bgroup "type assignment"
                       [ bench "check" $ nf runCheck p
                       , bench "assign" $ nf runAssign p
                       , bench "closedModule" $ nf (runSpecialize =<<) (runAssign p)
                       , bench "closure" $ nf (\m -> closure (m, mkModuleMap m)) (void <$> snd p)
                       , bench "shuttle" $ nf (uncurry monomorphize) p
+                      , bench "shuttle" $ nf (uncurry monomorphize) s
                       ]
                 ]
     where parsedM = yeetIO . parseWithMax =<< BSL.readFile "test/data/ty.kmp"
+          splitmix = yeetIO . parseWithMax =<< BSL.readFile "examples/splitmix.kmp"
+          forTyEnv = (,) <$> parsedM <*> splitmix
           yeetIO = either throwIO pure
           runCheck (maxU, m) = runTypeM maxU (checkModule m)
           runAssign (maxU, m) = runTypeM maxU (assignModule m)
