50fc097fd45cac4722409200bbf8cdc9da32775b
Vanessa McHale <vamchale@gmail.com>
2020-10-28 15:42:55 -0500

    More benchmarks for completeness

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -17,19 +17,22 @@ main =
                       [ bench "lex"   $ nf lexKempe contents
                       , bench "parse" $ nf parse contents
                       ]
-                , env forTyEnv $ \ ~(p, s) ->
+                , env forTyEnv $ \ ~(p, s, prel) ->
                     bgroup "type assignment"
-                      [ bench "check" $ nf runCheck p
-                      , bench "assign" $ nf runAssign p
+                      [ bench "check (test/data/ty.kmp)" $ nf runCheck p
+                      , bench "check (prelude/fn.kmp)" $ nf runCheck prel
+                      , bench "assign (test/data/ty.kmp)" $ nf runAssign p
+                      , bench "assign (prelude/fn.kmp)" $ nf runAssign prel
                       , bench "closedModule" $ nf (runSpecialize =<<) (runAssign p)
                       , bench "closure" $ nf (\m -> closure (m, mkModuleMap m)) (void <$> snd p)
-                      , bench "shuttle" $ nf (uncurry monomorphize) p
-                      , bench "shuttle" $ nf (uncurry monomorphize) s
+                      , bench "shuttle (test/data/ty.kmp)" $ nf (uncurry monomorphize) p
+                      , bench "shuttle (examples/splitmix.kmp)" $ nf (uncurry monomorphize) s
                       ]
                 ]
     where parsedM = yeetIO . parseWithMax =<< BSL.readFile "test/data/ty.kmp"
           splitmix = yeetIO . parseWithMax =<< BSL.readFile "examples/splitmix.kmp"
-          forTyEnv = (,) <$> parsedM <*> splitmix
+          prelude = yeetIO . parseWithMax =<< BSL.readFile "prelude/fn.kmp"
+          forTyEnv = (,,) <$> parsedM <*> splitmix <*> prelude
           yeetIO = either throwIO pure
           runCheck (maxU, m) = runTypeM maxU (checkModule m)
           runAssign (maxU, m) = runTypeM maxU (assignModule m)
