97d47d082d3ca8a261371247494ae7e6848c3cb2
Vanessa McHale <vanessa.mchale@bitnomial.com>
2020-11-03 11:59:22 -0600

    remove Push from IR

--- src/Kempe/Asm/X86.hs
+++ src/Kempe/Asm/X86.hs
@@ -52,7 +52,6 @@ irCosts :: IR.Stmt () -> IR.Stmt Int
 irCosts (IR.Eff _ e)     = let e' = expCostAnn e in IR.Eff (IR.expCost e') e'
 irCosts (IR.Jump _ l)    = IR.Jump 1 l
 irCosts (IR.KCall _ l)   = IR.KCall 2 l
-irCosts (IR.Push _ 8 e)  = let e' = expCostAnn e in IR.Push (1 + IR.expCost e') 8 e'
 irCosts (IR.Labeled _ l) = IR.Labeled 0 l
 
 -- does this need a monad for labels/intermediaries?
