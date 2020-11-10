50294bf2fb888cf16e82fb4bb9b3c44b6cd3987f
Vanessa McHale <vanessa.mchale@bitnomial.com>
2020-11-10 08:38:44 -0600

    module place

---
+++ src/Kempe/Asm/X86/Liveness.hs
@@ -0,0 +1,12 @@
+module Kempe.Asm.X86.Liveness ( initLiveness
+                              , Liveness
+                              ) where
+
+import Data.Functor (($>))
+import qualified Data.Set as S
+import Kempe.Asm.X86.Type
+
+type Liveness = S.Set AbsReg
+
+initLiveness :: [X86 AbsReg a] -> [X86 AbsReg Liveness]
+initLiveness = fmap ($> S.empty)
