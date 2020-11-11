1632a0ef7845a4d3ba6148ebf9574ccc2b0e4860
Vanessa McHale <vamchale@gmail.com>
2020-11-11 09:19:31 -0600

    might work

--- src/Kempe/Asm/X86/Linear.hs
+++ src/Kempe/Asm/X86/Linear.hs
@@ -1,8 +1,12 @@
 -- | Linear scan register allocator
+--
+-- See: https://web.stanford.edu/class/archive/cs/cs143/cs143.1128/lectures/17/Slides17.pdf
 module Kempe.Asm.X86.Linear ( X86Reg (..)
                             , allocReg
                             ) where
 
+import           Control.Monad.State (State, evalState)
+import qualified Data.Set            as S
 import           Kempe.Asm.X86.Type
 
 -- currently just has 64-bit and 8-bit registers
@@ -20,6 +24,18 @@ data X86Reg = Rax
             | CL
             | DH
             | DL
+            deriving (Eq, Ord, Enum, Bounded)
+
+-- set of free registers we iterate over
+type FreeSt = (S.Set X86Reg, S.Set X86Reg)
+
+allFree :: FreeSt
+allFree = (S.fromList [Rax .. Rdx], S.fromList [AH .. DL])
+
+type FreeM = State FreeSt
+
+runFreeM :: FreeM a -> a
+runFreeM = flip evalState allFree
 
 -- TODO: generate spill code
 allocReg :: [X86 AbsReg Liveness] -> [X86 X86Reg ()]
