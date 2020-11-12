72d2eb0a417c2c576f12c438bb97f4e4383f8049
Vanessa McHale <vanessa.mchale@bitnomial.com>
2020-11-12 11:19:30 -0600

    Assembler

--- src/Kempe/Asm/X86/Dyn.hs
+++ src/Kempe/Asm/X86/Dyn.hs
@@ -1,7 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+
 module Kempe.Asm.X86.Dyn ( assemble
                          ) where
 
 import Kempe.Asm.X86.Type
+import CodeGen.X86
+import Data.Foldable (traverse_)
 
-assemble :: [X86 X86Reg ()] -> a
-assemble = undefined
+assemble :: [X86 X86Reg ()] -> Code
+assemble = traverse_ asmInstruction
+
+asReg64 :: FromReg c => X86Reg -> c 'S64
+asReg64 Rax = rax
+asReg64 Rbx = rbx
+asReg64 Rcx = rcx
+asReg64 Rdx = rdx
+asReg64 Rsp = rsp
+asReg64 Rbp = rbp
+asReg64 _ = error "Internal error: not a 64-bit register!"
+
+asmInstruction :: X86 X86Reg () -> Code
+asmInstruction Ret{} = ret
+asmInstruction (PushReg _ r) = push (asReg64 r)
