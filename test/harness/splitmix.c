0f0114addb8267223a3aad384bb0a32f2ebd91a4
Vanessa McHale <vamchale@gmail.com>
2020-11-19 15:17:45 -0600

    Try?

--- test/harness/splitmix.c
+++ test/harness/splitmix.c
@@ -4,7 +4,7 @@
 extern uint64_t from_seed(uint64_t);
 
 // modified to have ""multiple return"" since C doesn't really have that
-uint64_t next(uint64_t x, uint64_t* y) {
+uint64_t next_(uint64_t x, uint64_t* y) {
 	uint64_t z = (x += 0x9e3779b97f4a7c15);
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
 	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
@@ -13,9 +13,8 @@ uint64_t next(uint64_t x, uint64_t* y) {
 }
 
 int main(int argc, char *argv[]) {
-    uint64_t* y;
-    uint64_t res = next(3012512025, y);
-    printf("%d", (int) res);
-    /* printf("%d", (int) from_seed(3012512025)); */
+    uint64_t y;
+    uint64_t res = next_(3012512025, &y);
+    printf("%u\n", (unsigned int) res);
+    printf("%u", (unsigned int) from_seed(3012512025));
 }
-
