e52c5a2f79acb892f9044395e6b221c37857ac68
Vanessa McHale <vamchale@gmail.com>
2020-11-21 18:55:11 -0600

    restore comments

--- src/Kempe/IR.hs
+++ src/Kempe/IR.hs
@@ -299,7 +299,7 @@ writeAtom (AtBuiltin ([i0, i1], _) Swap) =
         sz1 = size i1
     in
         pure $
-            copyBytes 0 (-sz0) sz0
+            copyBytes 0 (-sz0) sz0 -- copy i0 to end of the stack
                 ++ [ MovMem () (dataPointerAt (sz0 + sz1 - i)) 1 (Mem () 1 $ dataPointerAt (sz0 + i)) | i <- [0..(sz1-1)] ] -- copy i1 to where i0 used to be
                 ++ copyBytes (-sz0) 0 sz0 -- copy i0 at end of stack to its new place
 writeAtom (AtBuiltin _ Swap) = error "Ill-typed swap!"
@@ -319,7 +319,7 @@ dipify sz (AtBuiltin ([i0, i1], _) Swap) =
         sz1 = size i1
     in
         pure $
-            copyBytes 0 (-sz - sz0) sz0
+            copyBytes 0 (-sz - sz0) sz0 -- copy i0 to end of the stack
                 ++ [ MovMem () (dataPointerAt (sz + sz0 + sz1 - i)) 1 (Mem () 1 $ dataPointerAt (sz + sz0 + i)) | i <- [0..(sz1-1)] ] -- copy i1 to where i0 used to be
                 ++ copyBytes (-sz - sz0) (-sz) sz0 -- copy i0 at end of stack to its new place
 dipify _ (Dip ([], _) _) = error "Internal error: Ill-typed dip()!"
