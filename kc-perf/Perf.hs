40be7c88da1f257b58a30f89b7f0d4e34a44edef
Vanessa McHale <vamchale@gmail.com>
2020-11-26 16:47:10 -0600

    still sus idk

--- kc-perf/Perf.hs
+++ kc-perf/Perf.hs
@@ -12,6 +12,7 @@ main = do
     defaultMain [ bgroup "is_prime"
                       [ bench "2017" $ nf is_prime 2017
                       , bench "9719" $ nf is_prime 9719
+                      , bench "4294967296" $ nf is_prime 4294967296
                       , bench "4294967291" $ nf is_prime (2 ^ (32 :: Int) - 5)
                       ]
                 ]
