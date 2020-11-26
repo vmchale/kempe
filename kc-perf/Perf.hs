7b31c391305c7c446c5049b8f7714e90c1475f09
Vanessa McHale <vamchale@gmail.com>
2020-11-26 16:45:26 -0600

    looking wrong?

--- kc-perf/Perf.hs
+++ kc-perf/Perf.hs
@@ -1,5 +1,7 @@
 module Main (main) where
 
+-- https://primes.utm.edu/lists/2small/0bit.html
+
 import           Criterion.Main
 import           Foreign.C.Types
 
@@ -8,6 +10,8 @@ foreign import ccall unsafe is_prime :: CInt -> CBool
 main :: IO ()
 main = do
     defaultMain [ bgroup "is_prime"
-                      [ bench "2³² - 5" $ nf is_prime (2 ^ (32 :: Int) - 5)
+                      [ bench "2017" $ nf is_prime 2017
+                      , bench "9719" $ nf is_prime 9719
+                      , bench "4294967291" $ nf is_prime (2 ^ (32 :: Int) - 5)
                       ]
                 ]
