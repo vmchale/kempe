f8d25f2ff84fae8991c7ca7607e8a2630393fb3e
Vanessa McHale <vanessa.mchale@bitnomial.com>
2020-12-02 14:28:36 -0600

    Remove Data.Bifunctor.Ext

--- src/Data/Bifunctor/Ext.hs
+++
@@ -1,9 +0,0 @@
-module Data.Bifunctor.Ext ( (~<$)
-                          ) where
-
-import           Data.Bifunctor (Bifunctor (first))
-
-infixl 4 ~<$
-
-(~<$) :: Bifunctor p => a -> p b c -> p a c
-(~<$) x = first (const x)
