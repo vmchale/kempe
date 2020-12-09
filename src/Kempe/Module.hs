4f60fa14608ffda34b6b8d8cbf0755813a743b48
Vanessa McHale <vamchale@gmail.com>
2020-12-09 07:05:44 -0600

    somehow haven't broken anything

--- src/Kempe/Module.hs
+++ src/Kempe/Module.hs
@@ -4,22 +4,27 @@
 module Kempe.Module ( parseProcess
                     ) where
 
-import           Control.Arrow        ((***))
-import           Control.Exception    (Exception, throwIO)
-import qualified Data.ByteString.Lazy as BSL
-import           Data.Tuple.Extra     (fst3)
+import           Control.Exception          (Exception, throwIO)
+import qualified Data.ByteString.Lazy       as BSL
+import qualified Data.ByteString.Lazy.Char8 as ASCII
+import           Data.Tuple.Extra           (fst3, third3)
 import           Kempe.AST
 import           Kempe.Lexer
 import           Kempe.Parser
 
 parseProcess :: FilePath -> IO (Int, Declarations AlexPosn AlexPosn AlexPosn)
 parseProcess fp = do
-    contents <- BSL.readFile fp
-    (st, m) <- yeetIO $ parseWithInitCtx contents
-    pure $ (fst3 *** body) (st, m)
+    (st, [], ds) <- loopFps [fp] alexInitUserState
+    pure (fst3 st, ds)
 
 yeetIO :: Exception e => Either e a -> IO a
 yeetIO = either throwIO pure
+
+loopFps :: [FilePath] -> AlexUserState -> IO (AlexUserState, [FilePath], Declarations AlexPosn AlexPosn AlexPosn)
+loopFps [] st = pure (st, [], [])
+loopFps (fp:fps) st = do
+    (st', Module is ds) <- parseStep fp st
+    third3 (ds ++) <$> loopFps (fps ++ fmap ASCII.unpack is) st'
 
 parseStep :: FilePath -> AlexUserState -> IO (AlexUserState, Module AlexPosn AlexPosn AlexPosn)
 parseStep fp st = do
