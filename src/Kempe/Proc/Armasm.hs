30902c5320b380cef1169965288619ca8b3ce5c1
Vanessa McHale <vamchale@gmail.com>
2021-03-06 05:26:05 -0600

    no intermed

--- src/Kempe/Proc/Armasm.hs
+++ src/Kempe/Proc/Armasm.hs
@@ -1,21 +1,18 @@
 module Kempe.Proc.Armasm ( writeO
                          ) where
 
-import           Data.Functor              (void)
-import           Prettyprinter             (Doc, defaultLayoutOptions, layoutPretty)
-import           Prettyprinter.Render.Text (renderIO)
-import           System.IO                 (hFlush)
-import           System.IO.Temp            (withSystemTempFile)
-import           System.Process            (CreateProcess (..), StdStream (Inherit), proc, readCreateProcess)
+import           Data.Functor                (void)
+import           Prettyprinter               (Doc, defaultLayoutOptions, layoutPretty)
+import           Prettyprinter.Render.String (renderString)
+import           System.Process              (CreateProcess (..), StdStream (Inherit), proc, readCreateProcess)
 
 -- | Assemble using @armasm@, output in some file.
 writeO :: Doc ann
        -> FilePath
        -> Bool -- ^ Debug symbols?
        -> IO ()
-writeO p fpO dbg = withSystemTempFile "kmp.S" $ \fp h -> do
-    renderIO h (layoutPretty defaultLayoutOptions p)
-    hFlush h
-    let debugFlag = if dbg then ("-g":) else id
-    void $ readCreateProcess ((proc "nasm" (debugFlag [fp, fpO])) { std_err = Inherit }) ""
+writeO p fpO dbg = do
+    let inp = renderString (layoutPretty defaultLayoutOptions p)
+        debugFlag = if dbg then ("-g":) else id
+    void $ readCreateProcess ((proc "armasm" (debugFlag ["-o", fpO, "-"])) { std_err = Inherit }) inp
 
