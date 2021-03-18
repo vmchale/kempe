1076747b42cf7f9a65a79574e8b05da770e656d0
Vanessa McHale <vamchale@gmail.com>
2021-03-18 17:45:57 -0500

    fix lol

--- src/Kempe/Asm/Arm/Trans.hs
+++ src/Kempe/Asm/Arm/Trans.hs
@@ -3,7 +3,7 @@
 module Kempe.Asm.Arm.Trans ( irToAarch64
                            ) where
 
-import           Data.Bits          (shiftL, (.&.))
+import           Data.Bits          (shiftR, (.&.))
 import           Data.Foldable.Ext  (foldMapA)
 import           Data.Int           (Int64)
 import           Data.List          (scanl')
@@ -100,9 +100,9 @@ irEmit _ (IR.MJump e l) = do
 movRWord :: AbsReg -> Word -> [Arm AbsReg ()]
 movRWord r w = [MovRWord () r (fromIntegral b0), MovRK () r (fromIntegral b1) 16, MovRK () r (fromIntegral b2) 32, MovRK () r (fromIntegral b3) 48]
     where b0 = w .&. 0xFFFF
-          b1 = (w .&. 0xFFFF0000) `shiftL` 16
-          b2 = (w .&. 0xFFFF00000000) `shiftL` 32
-          b3 = (w .&. 0xFFFF000000000000) `shiftL` 48
+          b1 = (w .&. 0xFFFF0000) `shiftR` 16
+          b2 = (w .&. 0xFFFF00000000) `shiftR` 32
+          b3 = (w .&. 0xFFFF000000000000) `shiftR` 48
 
 evalE :: IR.Exp -> IR.Temp -> WriteM [Arm AbsReg ()]
 evalE (IR.ConstInt i) r                                                        = pure [MovRC () (toAbsReg r) i]
