793c9137e147b4d8acf17e5acd9c0d7f5b24be97
Vanessa McHale <vamchale@gmail.com>
2021-03-18 08:48:48 -0500

    move

--- src/Kempe/Asm/Arm/Type.hs
+++ src/Kempe/Asm/Arm/Type.hs
@@ -47,7 +47,7 @@ data AbsReg = DataPointer
 
 instance Pretty AbsReg where
     pretty DataPointer  = "datapointer"
-    pretty (AllocReg i) = "Abs" <> pretty i -- ₀₁₂₃₄₅₆₇₈₉
+    pretty (AllocReg i) = "Abs" <> pretty i
     pretty CArg0        = "X0"
     pretty CArg1        = "X1"
     pretty CArg2        = "X2"
