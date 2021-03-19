59a73c5a8606dd3377b196049e63b44105c33a24
Vanessa McHale <vamchale@gmail.com>
2021-03-19 12:34:53 -0500

    Use renderText first for smaller blocks/chunks?

--- src/Kempe/Proc/Nasm.hs
+++ src/Kempe/Proc/Nasm.hs
@@ -2,9 +2,9 @@ module Kempe.Proc.Nasm ( writeO
                        ) where
 
 import           Data.Functor              (void)
+import qualified Data.Text.Lazy.IO         as TLIO
 import           Prettyprinter             (Doc, defaultLayoutOptions, layoutPretty)
-import           Prettyprinter.Render.Text (renderIO)
-import           System.IO                 (hFlush)
+import           Prettyprinter.Render.Text (renderLazy)
 import           System.IO.Temp            (withSystemTempFile)
 import           System.Process            (CreateProcess (..), StdStream (Inherit), proc, readCreateProcess)
 
@@ -14,8 +14,8 @@ writeO :: Doc ann
        -> Bool -- ^ Debug symbols?
        -> IO ()
 writeO p fpO dbg = withSystemTempFile "kmp.S" $ \fp h -> do
-    renderIO h (layoutPretty defaultLayoutOptions p)
-    hFlush h
+    let txt = renderLazy $ layoutPretty defaultLayoutOptions p
+    TLIO.hPutStr h txt
     let debugFlag = if dbg then ("-g":) else id
     -- -O1 is signed byte optimization but no multi-passes
     void $ readCreateProcess ((proc "nasm" (debugFlag [fp, "-f", "elf64", "-O1", "-o", fpO])) { std_err = Inherit }) ""
