12cf70ecf28eefb0621f3ba0fff2f71d76434b4c
Vanessa McHale <vamchale@gmail.com>
2021-09-17 13:46:34 -0500

    temp

--- test/harness/succ.c
+++ test/harness/succ.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-extern int succ0(int);
-extern int succ1(int);
+/* extern int succ0(int); */
+/* extern int succ1(int); */
+extern int succ2(int);
 
 int main(int argc, char *argv[]) {
-    printf("%d", succ0(4));
-    printf("%d", succ1(4));
+    /* printf("%d\n", succ0(4)); */
+    /* printf("%d\n", succ1(4)); */
+    printf("%d\n", succ2(4));
 }
