c58ec20302e399f28501075c101037eb25ce8468
Vanessa McHale <vamchale@gmail.com>
2021-09-25 16:41:12 -0500

    add lints for various commutative binary operators + operators that have a reflect

--- src/Kempe/Check/Lint.hs
+++ src/Kempe/Check/Lint.hs
@@ -17,6 +17,21 @@ lintDecl ExtFnDecl{}          = Nothing
 lintDecl (FunDecl _ _ _ _ as) = lintAtoms as
 
 lintAtoms :: [Atom b b] -> Maybe (Warning b)
-lintAtoms []                       = Nothing
-lintAtoms (a@(Dip l _):a'@Dip{}:_) = Just (DoubleDip l a a')
-lintAtoms (_:as)                   = lintAtoms as
+lintAtoms []                                                = Nothing
+lintAtoms (a@(Dip l _):a'@Dip{}:_)                          = Just (DoubleDip l a a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ IntEq):_)     = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ IntNeq):_)    = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ And):_)       = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ Or):_)        = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ Xor):_)       = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ WordXor):_)   = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ IntTimes):_)  = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ IntPlus):_)   = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ WordPlus):_)  = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ WordTimes):_) = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin _ IntXor):_)    = Just (SwapBinary l a' a')
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin l' IntGt):_)    = Just (SwapBinary l a' (AtBuiltin l' IntLt))
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin l' IntGeq):_)   = Just (SwapBinary l a' (AtBuiltin l' IntLeq))
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin l' IntLt):_)    = Just (SwapBinary l a' (AtBuiltin l' IntGt))
+lintAtoms ((AtBuiltin l Swap):a'@(AtBuiltin l' IntLeq):_)   = Just (SwapBinary l a' (AtBuiltin l' IntGeq))
+lintAtoms (_:as)                                            = lintAtoms as
--- src/Kempe/Error/Warning.hs
+++ src/Kempe/Error/Warning.hs
@@ -8,14 +8,16 @@ import           Data.Semigroup    ((<>))
 import           Data.Typeable     (Typeable)
 import           Kempe.AST
 import           Kempe.Name
-import           Prettyprinter     (Pretty (pretty), (<+>))
+import           Prettyprinter     (Pretty (pretty), squotes, (<+>))
 
 data Warning a = NameClash a (Name a)
                | DoubleDip a (Atom a a) (Atom a a)
+               | SwapBinary a (Atom a a) (Atom a a)
 
 instance Pretty a => Pretty (Warning a) where
-    pretty (NameClash l x)    = pretty l <> " '" <> pretty x <> "' is defined more than once."
-    pretty (DoubleDip l a a') = pretty l <+> pretty a <+> pretty a' <+> "could be written as a single dip()"
+    pretty (NameClash l x)     = pretty l <> " '" <> pretty x <> "' is defined more than once."
+    pretty (DoubleDip l a a')  = pretty l <+> pretty a <+> pretty a' <+> "could be written as a single dip()"
+    pretty (SwapBinary l a a') = pretty l <+> squotes ("swap" <+> pretty a) <+> "is" <+> pretty a'
 
 instance (Pretty a) => Show (Warning a) where
     show = show . pretty
