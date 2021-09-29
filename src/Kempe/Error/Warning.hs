f75419c160ac2d83ad7d12ecf29d57f00e76529a
Vanessa McHale <vamchale@gmail.com>
2021-09-29 04:41:06 -0500

    add lints

--- src/Kempe/Error/Warning.hs
+++ src/Kempe/Error/Warning.hs
@@ -15,13 +15,15 @@ data Warning a = NameClash a (Name a)
                | SwapBinary a (Atom a a) (Atom a a)
                | DoubleSwap a
                | DipAssoc a (Atom a a)
+               | Identity a (Atom a a)
 
 instance Pretty a => Pretty (Warning a) where
     pretty (NameClash l x)     = pretty l <> " '" <> pretty x <> "' is defined more than once."
     pretty (DoubleDip l a a')  = pretty l <+> pretty a <+> pretty a' <+> "could be written as a single dip()"
     pretty (SwapBinary l a a') = pretty l <+> squotes ("swap" <+> pretty a) <+> "is" <+> pretty a'
     pretty (DoubleSwap l)      = pretty l <+> "double swap"
-    pretty (DipAssoc l a) = pretty l <+> "dip(" <> pretty a <> ")" <+> pretty a <+> "is equivalent to" <+> pretty a <+> pretty a <+> "by associativity"
+    pretty (DipAssoc l a)      = pretty l <+> "dip(" <> pretty a <> ")" <+> pretty a <+> "is equivalent to" <+> pretty a <+> pretty a <+> "by associativity"
+    pretty (Identity l a)      = pretty l <+> squotes ("dup" <+> pretty a) <+> "is identity"
 
 instance (Pretty a) => Show (Warning a) where
     show = show . pretty
