bb9a2ccdd990501d42ed4cc9571f88e57be910b2
Vanessa McHale <vamchale@gmail.com>
2021-10-08 08:45:10 -0500

    test div

--- test/harness/mod.c
+++ test/harness/mod.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 extern int mod_kmp(int, int);
+extern int div_kmp(int, int);
 
 int main(int argc, char *argv[]) {
     printf("%d\n", mod_kmp(2, -3));
+    printf("%d\n", div_kmp(2, -3));
 }
