747f18eeb214b15c4ddd675d8b4547a892d184c5
Vanessa McHale <vamchale@gmail.com>
2024-04-16 09:41:24 -0400

    profiling flags

--- src/Kempe/Proc/Nasm.hs
+++ src/Kempe/Proc/Nasm.hs
@@ -16,8 +16,8 @@ writeO :: Doc ann
        -> IO ()
 writeO p fpO dbg = withSystemTempFile "kmp.S" $ \fp h -> do
     let txt = renderLazy $ layoutCompact p
-    TLIO.hPutStr h txt
-    hFlush h
+    {-# SCC "Prettyprinter" #-} TLIO.hPutStr h txt
+    {-# SCC "Prettyprinter" #-} hFlush h
     let debugFlag = if dbg then ("-g":) else id
     -- -O1 is signed byte optimization but no multi-passes
-    void $ readCreateProcess ((proc "nasm" (debugFlag [fp, "-f", "elf64", "-O1", "-o", fpO])) { std_err = Inherit }) ""
+    {-# SCC "nasm" #-} void $ readCreateProcess ((proc "nasm" (debugFlag [fp, "-f", "elf64", "-O1", "-o", fpO])) { std_err = Inherit }) ""
